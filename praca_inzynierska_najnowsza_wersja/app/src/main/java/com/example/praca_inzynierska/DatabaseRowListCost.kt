926fd133f6e8f82e2f6f8cd93d971e70b42bf8b3
domstechly <sdominos@wp.pl>
2022-12-23 16:25:37 +0100

    poprawa plikow

--- praca_inzynierska_najnowsza_wersja/app/src/main/java/com/example/praca_inzynierska/DatabaseRowListCost.kt
+++
@@ -1,3 +0,0 @@
-package com.example.praca_inzynierska
-//klasa danych do bazy realtime database w firebase
-data class DatabaseRowListCost(val id:String="", val nazwa:String="", val value:Int=0, val typ:String="")
